c2068586516e8c672976978a65df4ae454dc3df9
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-04-21 19:27:53 +0530

    default_response and maximum_similarity_threshold added

--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,25 @@ from django.http import HttpResponse
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-bot = ChatBot('chatbot', read_only=False, logic_adapters=['chatterbot.logic.BestMatch'])
+bot = ChatBot('chatbot', read_only=False, 
+                logic_adapters=[
+                    {
+                        'import_path': 'chatterbot.logic.BestMatch',
+                        'default_response': 'Sorry, I dont know what that means',
+                        'maximum_similarity_threshold': 0.90
+                    }])
 
 list_to_train = [
-    "hi",
-    "hi, there",
+    "Hi",
+    "Hi, there",
     "What's your name?",
     "I am just a chatbot",
     "What is your fav food?",
-    "I like cheese"
+    "I like cheese",
+    "What's your fav sport?",
+    "Swimming",
+    "Do you have children?",
+    "No"
 ]
 
 list_trainer = ListTrainer(bot)
