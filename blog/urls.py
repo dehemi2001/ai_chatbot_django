f325469124f00dbd319c15a3585e7ed58eec84c0
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-04-21 17:05:51 +0530

    Getting user input to the backend

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('getResponse', views.getResponse, name='getResponse')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+def getResponse(request):
+    userMessage = request.GET.get('userMessage')
+    return HttpResponse(userMessage)
