e73d3f35614eb78cb50a931d0c5d425c293bf5df
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-04-22 11:12:06 +0530

    ChatterBotCorpusTrainer implemented

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
 bot = ChatBot('chatbot', read_only=False, 
                 logic_adapters=[
                     {
                         'import_path': 'chatterbot.logic.BestMatch',
-                        'default_response': 'Sorry, I dont know what that means',
-                        'maximum_similarity_threshold': 0.90
-                    }])
+                        # 'default_response': 'Sorry, I dont know what that means',
+                        # 'maximum_similarity_threshold': 0.90
+                    }
+                    ])
 
 list_to_train = [
     "Hi",
@@ -27,8 +29,12 @@ list_to_train = [
     "No"
 ]
 
-list_trainer = ListTrainer(bot)
-list_trainer.train(list_to_train)
+chatterbotCorpusTrainer = ChatterBotCorpusTrainer(bot)
+
+# list_trainer = ListTrainer(bot)
+# list_trainer.train(list_to_train)
+
+chatterbotCorpusTrainer.train('chatterbot.corpus.english')
 
 def index(request):
     return render(request, 'blog/index.html')
